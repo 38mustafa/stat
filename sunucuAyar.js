9c4f8c7c085b1a8d653abb02c4ca0eb024a68475
38mustafa <74008609+38mustafa@users.noreply.github.com>
2021-05-04 23:47:32 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '747756641784692797', // sunucu idsi
+  ownerRole:"747769744693919834", // sunucu sahiplerinin rolü
+  teyitsizRolleri:["789517694445027359"], // kayıt olmayanlara verieln rol
+  jailRolu:"789575348941094912", // cezalılara verilen rol
+  enAltYetkiliRolu:["789519235214606357"], // ilk yetkili permi
+  activity:"The Kraliyet ❤", // botun durumu
+  status:"online",
+  invitelink:"https://discord.gg/SPQFcp7E6B", //davet linki
+  symbol:"๘", // tag
+  voicechannel:"800100687270117398" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
